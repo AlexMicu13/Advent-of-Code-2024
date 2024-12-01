606b51ffa387486cf04e7814c5cea18a120d21e8
AlexMicu13 <alexandru.micu1206@stud.acs.upb.ro>
2024-12-01 23:43:07 +0200

    Day 1 Counter

--- Day 1/sol.py
+++ Day 1/sol.py
@@ -1,13 +1,18 @@
+from collections import Counter
+
 left = []
 right = []
+right_count = Counter()
 with open('input.txt') as f:
 	for line in f:
 		e1, e2 = line.split()
-		left.append(int(e1))
-		right.append(int(e2))
+		e1, e2 = int(e1), int(e2)
+		left.append(e1)
+		right.append(e2)
+		right_count[e2] += 1
 
-s2 = sum(x * right.count(x) for x in left)
 left.sort()
 right.sort()
 s1 = sum(abs(l - r) for l, r in zip(left, right))
+s2 = sum(x * right_count.get(x, 0) for x in left)
 print(f'Answer 1: {s1}\nAnswer 2: {s2}')
